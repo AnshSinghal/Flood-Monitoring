57bf6e40d4def3f36092fb29abb8ac6199102416
AnshSinghal <potterheadansh@gmail.com>
2025-06-25 14:30:52 +0000

    scripts for model training

--- src/train.py
+++ src/train.py
@@ -1,25 +1,60 @@
 import torch
-import torch.utils.data as DataLoader
+import yaml
+from argparse import ArgumentParser, Namespace
 from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import WandBLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-from argparse import ArgumentParser
+from pytorch_lightning.loggers import WandBLogger
 
 from lightning_module import SAR2OpticalGAN
-from dataset import FloodDataset
+from datamodule import SARDataModule
+
+from model import UNetGenerator, PatchGANDiscriminator
+from losses import PerceptualLoss, LSGANLoss, SpecklePreservationLoss, WaterIndexConsistencyLoss
+import pytorch_lightning as pl
 
 def main(hparams):
-    #Initialize Model
-    model = SAR2OpticalGAN(hparams)
-
-    #initialize dataset and loaders
-    train_dataset = FloodDataset(manifest_path=hparams.manifest_path, split='train')
-    val_dataset = FloodDataset(manifest_path=hparams.manifest_path, split='val')
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=hparams.batch_size,
-        shuffle=True,
-        num_workers=hparams.num_workers,
-        pin_memory=True
+    wandb_logger = WandBLogger(
+        project=hparams.project_name,
+        name=hparams.run_name,
     )
+
+    wandb_logger.log_hyperparams(hparams)
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=f"checkpoints/{hparams.run_name}",
+        filename='{epoch:02d}-{val_psnr:.2f}',
+        save_top_k=3,
+        verbose=True,
+        monitor='val/psnr', # Monitor a validation metric like PSNR
+        mode='max'
+    )
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    model = SAR2OpticalGAN(hparams)
+    datamodule = SARDataModule(hparams)
+
+    trainer = Trainer(
+        max_epochs=hparams.max_epochs,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback, lr_monitor],
+        accelerator=hparams.accelerator,
+        devices=1,
+        precision=hparams.precision,
+        log_every_n_steps=hparams.log_every_n_steps,
+    )
+    trainer.fit(model, datamodule)
+
+    wandb_logger.experiment.finish()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--config', type=str, required=True, help='Path to the YAML configuration file.')
+    args = parser.parse_args()
+
+
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    hparams = Namespace(**config)
+    main(hparams)
